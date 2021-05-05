de6ef058d17e36bbdfa131a2501a46f017e6222f
Zsofia von Gordon <zsofiavongordon@Piris-Air.latex-01>
2021-05-05 14:36:50 -0400

    Co-authored-by: Edward Feng <edwfen22@bergen.org> Co-authored-by: Anthony Li <me@anli.dev>

---
+++ useragent/server/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { readFileSync } = require("fs");
+const path = require("path");
+
+const app = express();
+//const flag = readFileSync("flag.txt", "utf-8");
+
+app.get("/",function (request,response){
+    const agent = request.get("User-Agent");
+    var userthing;
+
+    if (/gerald/.test(agent.toLowerCase())) {
+        userthing = `<!DOCTYPE html>
+        <html>
+            <head>
+            </head>
+            <body>
+                <h1>Welcome to the Stegosaurus Intelligence-6 Homepage</h1>
+                <h2>Are you Agent Gerald?</h2>
+                <img src="gerald.PNG" alt="agent gerald" style="width: 50%"></img>
+                   <h4> Welcome, Agent Gerald! Your flag is: bcactf{y0u_h@ck3d_5tegos@urus_1nt3lligence} </h4>
+            </body>
+        </html>`;
+
+    } else {
+        userthing = `<!DOCTYPE html>
+        <html>
+            <head>
+            </head>
+            <body>
+                <h1>Welcome to the Stegosaurus Intelligence-6 Homepage</h1>
+                <h2>Are you Agent Gerald?</h2>
+                <img src="gerald.PNG" alt="agent gerald" style="width: 50%"></img>
+                <h4> You're not Agent Gerald...</h4>
+            </body>
+        </html>`;
+    }
+
+    response.send(no_u);
+        
+
+});
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(2562);
+console.log("Listening on http://localhost:2562");
+
+
+
+
