c58e1cb10343bd4611e50506a0fc7a66c53e394e
MichaelK522 <michaelk522@Michaels-PC.localdomain>
2021-05-11 13:27:29 -0400

    Initial commit

---
+++ wait_this_isnt_c/flag_checker_1.f90
@@ -0,0 +1,27 @@
+program flag_checker_1
+    implicit none
+
+    integer(kind = 2) :: i
+    character (len = 25) :: input_flag
+    integer(kind = 2), dimension(25) :: input_flag_int, compare_flag
+    compare_flag = (/99,101,100,103,121,108,130,110,57,124,127,126,65,92,110, &
+    121, 70, 113, 118, 68, 132, 101, 71, 132, 150/)
+
+    print *, "Please enter a flag: "
+    read *, input_flag
+
+    do i = 1, 20
+      input_flag_int(i) = ichar(input_flag(i:i+1), 2)
+    end do
+
+    do i = 1,20
+      input_flag_int(i) = input_flag_int(i) + i
+      if (input_flag_int(i) .ne. compare_flag(i)) then
+        print *, "Sorry, flag does not match."
+        exit
+      end if
+    end do
+
+    print *, "Congrats, that was the flag!"
+
+end program flag_checker_1
