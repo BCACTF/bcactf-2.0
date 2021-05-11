7206c4fb8a8c629151ca1da992b4b3144492b830
MichaelK522 <michaelk522@Michaels-PC.localdomain>
2021-05-11 18:25:30 -0400

    fix code

--- wait_this_isnt_c/flag_checker_1.f90
+++ wait_this_isnt_c/flag_checker_1.f90
@@ -10,15 +10,15 @@ program flag_checker_1
     print *, "Please enter a flag: "
     read *, input_flag
 
-    do i = 1, 20
+    do i = 1, 25
       input_flag_int(i) = ichar(input_flag(i:i+1), 2)
     end do
 
-    do i = 1,20
+    do i = 1,25
       input_flag_int(i) = input_flag_int(i) + i
       if (input_flag_int(i) .ne. compare_flag(i)) then
         print *, "Sorry, flag does not match."
-        exit
+        call EXIT(1)
       end if
     end do
 
