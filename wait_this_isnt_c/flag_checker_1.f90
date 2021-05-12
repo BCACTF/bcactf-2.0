a244b7efa95982e3c19cf83ee16af153ba4686fb
MichaelK522 <michaelk522@Michaels-PC.localdomain>
2021-05-11 22:00:07 -0400

    update how the array is made

--- wait_this_isnt_c/flag_checker_1.f90
+++ wait_this_isnt_c/flag_checker_1.f90
@@ -4,8 +4,31 @@ program flag_checker_1
     integer(kind = 2) :: i
     character (len = 25) :: input_flag
     integer(kind = 2), dimension(25) :: input_flag_int, compare_flag
-    compare_flag = (/99,101,100,103,121,108,130,110,57,124,127,126,65,92,110, &
-    121, 70, 113, 118, 68, 132, 101, 71, 132, 150/)
+    compare_flag(1) = 99
+    compare_flag(2) = 101
+    compare_flag(3) = 100
+    compare_flag(4) = 103
+    compare_flag(5) = 121
+    compare_flag(6) = 108
+    compare_flag(7) = 130
+    compare_flag(8) = 110
+    compare_flag(9) = 57
+    compare_flag(10) = 124
+    compare_flag(11) = 127
+    compare_flag(12) = 126
+    compare_flag(13) = 65
+    compare_flag(14) = 92
+    compare_flag(15) = 110
+    compare_flag(16) = 121
+    compare_flag(17) = 70
+    compare_flag(18) = 113
+    compare_flag(19) = 118
+    compare_flag(20) = 68
+    compare_flag(21) = 132
+    compare_flag(22) = 101
+    compare_flag(23) = 71
+    compare_flag(24) = 132
+    compare_flag(25) = 150
 
     print *, "Please enter a flag: "
     read *, input_flag
