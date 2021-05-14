0fab4126378257ef05f3253fb310cbd62258134b
MichaelK522 <michaelk522@Michaels-PC.localdomain>
2021-05-13 23:38:22 -0400

    update to not use sqrt matrix

--- wait_this_isnt_c_2/flag_checker_2.f90
+++ wait_this_isnt_c_2/flag_checker_2.f90
@@ -4,35 +4,37 @@ program flag_checker_2
     integer(kind = 4) :: i, j, k
     character (len = 25) :: input_flag
     integer(kind = 4), dimension(5,-2:2) :: matrix_a, matrix_b, compare_flag
-    compare_flag(1,-2) = 39739
-    compare_flag(1,-1) = 52036
-    compare_flag(1, 0) = 54026
-    compare_flag(1, 1) = 47817
-    compare_flag(1, 2) = 52096
+    integer(kind = 4), dimension(0:4, 0:4) :: matrix_c
 
-    compare_flag(2,-2) = 31525
-    compare_flag(2,-1) = 41848
-    compare_flag(2, 0) = 44574
-    compare_flag(2, 1) = 38500
-    compare_flag(2, 2) = 42085
+    compare_flag(1,-2) = 2752
+    compare_flag(1,-1) = 3290
+    compare_flag(1, 0) = 4041
+    compare_flag(1, 1) = 5221
+    compare_flag(1, 2) = 7022
 
-    compare_flag(3,-2) = 32329
-    compare_flag(3,-1) = 44352
-    compare_flag(3, 0) = 46438
-    compare_flag(3, 1) = 40818
-    compare_flag(3, 2) = 47399
+    compare_flag(2,-2) = 1913
+    compare_flag(2,-1) = 2348
+    compare_flag(2, 0) = 2977
+    compare_flag(2, 1) = 3998
+    compare_flag(2, 2) = 5570
 
-    compare_flag(4,-2) = 29383
-    compare_flag(4,-1) = 33197
-    compare_flag(4, 0) = 37103
-    compare_flag(4, 1) = 34311
-    compare_flag(4, 2) = 35123
+    compare_flag(3,-2) = 2627
+    compare_flag(3,-1) = 3088
+    compare_flag(3, 0) = 3685
+    compare_flag(3, 1) = 4620
+    compare_flag(3, 2) = 6110
 
-    compare_flag(5,-2) = 34428
-    compare_flag(5,-1) = 47089
-    compare_flag(5, 0) = 48032
-    compare_flag(5, 1) = 41921
-    compare_flag(5, 2) = 46292
+    compare_flag(4,-2) = 2137
+    compare_flag(4,-1) = 2517
+    compare_flag(4, 0) = 3010
+    compare_flag(4, 1) = 3848
+    compare_flag(4, 2) = 5077
+
+    compare_flag(5,-2) = 2434
+    compare_flag(5,-1) = 2910
+    compare_flag(5, 0) = 3580
+    compare_flag(5, 1) = 4608
+    compare_flag(5, 2) = 6203
 
     print *, "Please enter a flag: "
     read *, input_flag
@@ -55,7 +57,33 @@ program flag_checker_2
 
     matrix_a = cshift(matrix_a, shift = 2)
 
-    matrix_b = matmul(matrix_a, matrix_a)
+    i = 1
+    do j = 0,4
+      do k = 0,j
+        matrix_c(j,k) = i
+        matrix_c(k,j) = matrix_c(j,k)
+        i = i+1
+      end do
+    end do
+
+    ! do i = 0,4
+    !   do j = 0,4
+    !     write (*,*) matrix_c(i,j)
+    !   end do
+    ! end do
+
+    matrix_b = matmul(matrix_a, matrix_c)
+
+    ! print *, lbound(matrix_b)
+    ! print *, shape(matrix_b)
+    ! print *, ubound(matrix_b)
+    !
+    !
+    ! do i = 1,5
+    !   do j = -2,2
+    !     write (*,*) matrix_b(i,j)
+    !   end do
+    ! end do
 
     if ( all(matrix_b .eq. compare_flag) ) then
       print *, "Congrats, that was the flag!"
