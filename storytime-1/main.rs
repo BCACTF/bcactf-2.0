66c2c7b6ba0dae87d740207b231cef095baac559
micpap25 <micpap25@gmail.com>
2021-05-18 09:46:01 -0400

    Revisions to Storytime 1

--- storytime-1/main.rs
+++ storytime-1/main.rs
@@ -1,7 +1,8 @@
+use std::{thread, time};
 
 fn main() {
 
-    let lines = 4;
+    let lines = 12;
 
     let flag = "bcactf{w0ol_m4k3s_str1ng_ziv4mk3ca91b}";
 
@@ -13,10 +14,14 @@ fn main() {
     "One for the master,",
     "One for the dame,",
     "And one for the little boy",
-    "Who lives down the lane\n", 
-    flag];
+    "Who lives down the lane\n",
+    flag,
+    "",
+    "Did you know? I almost used \"Little Miss Muffet\" for this problem.", 
+    "Spiders make string too, kind of."];
 
     for i in 0..lines {
         println!("{}", lyrics[i]);
+        thread::sleep(time::Duration::from_millis(1500));
     }
 }
