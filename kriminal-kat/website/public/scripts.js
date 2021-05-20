c37fb392b66a8019eef42d11e618e37e60969f14
micpap25 <micpap25@gmail.com>
2021-05-20 17:54:44 -0400

    Working on this

--- kriminal-kat/website/public/scripts.js
+++ kriminal-kat/website/public/scripts.js
@@ -1,23 +1,24 @@
-const socket = io('http://localhost:9000', {
-    query: {
-        username
-    }
-})
+const socket = io("http://localhost:9000");
 
 socket.on("username", (username) => {
-    document.getElementById("username").setAttribute("value", username)
-})
+  document.getElementById("username").setAttribute("value", username);
+});
 
-socket.on("signInResponse", (type, content) => {
-    if (type === "success") {
-        document.getElementById("success").innerHTML = content;
-        document.getElementById("error").innerHTML = "";
-    } else {
-        document.getElementById("error").innerHTML = content;
-        document.getElementById("success").innerHTML = "";
-    }
-})
+// Doesn't recieve this for some reason???
+socket.on("signInResponse", (content) => {
+    setTimeout(console.log("Recieved SignIn response: " + content), 1000);
+  
+  
+//   if (type == "success") {
+//     document.getElementById("success").innerHTML = content;
+//     document.getElementById("error").innerHTML = "";
+//   } else {
+//     document.getElementById("error").innerHTML = content;
+//     document.getElementById("success").innerHTML = "";
+//   }
+});
 
 function submitForm() {
-    
+  console.log(document.getElementById("password").value.trim());
+  socket.emit("signInAttempt", document.getElementById("password").value.trim());
 }
--- kriminal-kat/website/server.js
+++ kriminal-kat/website/server.js
@@ -4,9 +4,8 @@ const md5 = require("md5");
 
 const app = express();
 const socketio = require("socket.io");
-const { fileURLToPath } = require("url");
 app.use(express.static(__dirname + "/public"));
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 const server = app.listen(port);
 const io = socketio(server);
 
@@ -16,28 +15,36 @@ const usernames = usernameFile.split("\n");
 const flag = readFileSync("flag.txt", "utf-8");
 
 io.on("connection", (socket) => {
-  const username = usernames[Math.floor(Math.random() * usernames.length)];
+  let username = usernames[Math.floor(Math.random() * usernames.length)].trim();
   socket.emit("username", username);
   socket.on("signInAttempt", (password) => {
-    if (username === password) {
+    console.log("recieved password: " + password);
+    console.log("username is: " + username);
+    if (username == password) {
+      console.log("username and password are equal");
       socket.emit(
         "signInResponse",
-        "failure",
+        // "failure",
         "Nice try, but your username and password can't be the same!"
       );
-    } else if (md5(username) === md5(password)) {
+      console.log("successfully emitted case 1");
+    } else if (md5(username) == md5(password)) {
+      console.log("Correct answer");
       socket.emit(
         "signInResponse",
-        "success",
+        // "success",
         "You signed in successfully! Agent Gerald found this in the chat logs:\n" +
           flag
       );
+      console.log("successfully emitted case 2");
     } else {
+      console.log("Wrong answer");
       socket.emit(
         "signInResponse",
-        "failure",
+        // "failure",
         "Incorrect Password! You might not be " + username + ", reanalyzing..."
       );
+      console.log("successfully emitted case 3");
       username = usernames[Math.floor(Math.random() * usernames.length)];
       socket.emit("username", username);
     }
