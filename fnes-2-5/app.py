39a80d9ac775aa36afff400353d83712d860f350
Anthony Li <me@anli.dev>
2021-06-02 23:25:43 -0400

    Separate usernames and admin

--- fnes-2-5/app.py
+++ fnes-2-5/app.py
@@ -1,25 +1,30 @@
 from flask import Flask, render_template, request, redirect, make_response
+from json import loads, dumps
 
 app = Flask(__name__)
 
 with open("flag.txt", "r") as file:
     flag = file.read()
 
-def check_token(token: str) -> str:
-    # TODO: Return username for token string, or just throw an error if something's wrong
-    return "Not Implemented"
+def check_token(token: str):
+    # TODO: Set contents to contents of token, or just throw an error if something's wrong
+    contents = '{"name":"vampire","admin":false}'
+    return loads(contents)
 
 @app.route("/")
 def home():
-    username = None
+    user = None
     token = request.cookies.get("enterprise-grade-token")
     if token is not None:
-        username = check_token(token)
-    return render_template("home.html", username=username)
+        user = check_token(token)
+        return render_template("home.html", username=user["name"], is_admin=user["admin"])
+    else:
+        return render_template("home.html")
 
 @app.route("/login")
 def login():
-    # TODO: Generate a token for the username ("guest" or "vampire" or something)
+    contents = dumps({"name": "vampire", "admin": False})
+    print(contents)
     token = ""
     response = redirect("/", code=303)
     response.set_cookie("enterprise-grade-token", token)
@@ -36,8 +41,8 @@ def turn_flagmachine_on():
     token = request.cookies.get("enterprise-grade-token")
     if token is None:
         return render_template("flagmachine.html", message="At Generic Enterprise™, We Care about Security and Privacy. That's why we require you to Authenticate™ first before modifying any Enterprise™ Industrial™-Grade Settings™.")
-    username = check_token(token)
-    if username == "admin":
+    user = check_token(token)
+    if user["admin"]:
         return render_template("flagmachine.html", message=flag)
     else:
         return render_template("flagmachine.html", message="At Generic Enterprise™, We Care about Security and Privacy. That's why only Authorized™ Personnel™ are permitted to modify our Enterprise™ Industrial™-Grade Settings™."), 403
