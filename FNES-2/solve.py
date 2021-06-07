92541765274e0e3660f1b177c2f20fe9cfa6625f
eiis1000 <erez.m.israeli@gmail.com>
2021-06-07 17:36:18 -0400

    Finish solve.py

--- FNES-2/solve.py
+++ FNES-2/solve.py
@@ -7,14 +7,16 @@ from Crypto.Cipher import AES
 from Crypto.Hash import SHA
 from Crypto.Util.Padding import pad, unpad
 
-
+"""
 with open("flag.txt", "r") as f:
     flag = f.read().strip().encode("ascii")
 
 with open("key.txt", "r") as f:
     key = int(f.read().strip())
-
+"""
 target_query = "Open sesame... Flag please!"
+key = 1234
+flag = b"test flag"
 
 print("""
 Welcome to your new and improved FNES... FNES 2!
@@ -28,7 +30,9 @@ Here are the steps:
 PS: For security reasons, there are still some characters you aren't allowed to encrypt. Sorry!
 """)
 
-tempkey = SHA.new(int(key + int(time.time() / 10)).to_bytes(64, 'big')).digest()[0:16]
+
+
+tempkey = SHA.new(int(key).to_bytes(64, 'big')).digest()[0:16]
 
 
 def decrypt(I):
@@ -61,39 +65,62 @@ def TD(I):
     except ValueError:
         return -1
 
+
+
+""" # modified not actually true anymore
+>>> nn = int(binascii.hexlify(pad(b'Open sesame... Flag please!',16)[:16]),16)
+144062367998332532977264151438989526277
+>>> oo = int('10'*16,16)
+>>> mm = int('1331358226112515603a80f4b23f38ee',16)
+24578579110157967680423669472281668203
+>>> nn
+144062367998332532977264151438989526277
+>>> oo
+21351050473470648687898014583954083856
+>>> mm ^ nn ^ oo
+146281903957790059843686435045030215550
+>>> hex(_)
+'0x6e0cdeb2c70bf1f17468069bbf93bf7e'
+>>> TD(b'0'*32+b'6e0cdeb2c70bf1f17468069bbf93bf7e'+b'0'*32)
+b'\xf1"\xa3\xec\xf1b^\x06[\x86\xa0j\xcde\xc2:lag please!\x05\x05\x05\x05\x05'
+"""
+
+'dee784a384e65433932fd3b6b88af257'
+'4c5140fc167250761147f5ca8c0108b8' #goes before
+#final string:
+b'4c5140fc167250761147f5ca8c0108b8dee784a384e65433932fd3b6b88af257' + b'0'*32
+
+
+
 def brk():
-    c = b'1'*64 + b'0'
+    c = b'0'*32 + b'dee784a384e65433932fd3b6b88af257' + b'0'#*32 + b'0'
     for i in range(16):
         ret = -1
         i = i + 16
-        while ret == -1:
-            print(c)
-            print(TD(c[:-1]))
+        for k in range(256):
+            print((c[-65:-33],binascii.hexlify(TD(c[:-1]))))
             ret = decrypt(c[:-1])
             if ret == -1:
                 t = c[-3 + -2*i:-2 + -2*i] + c[-2 + -2*i:-1 + -2*i]
-                t = (hex(int(str(t)[2:-1],16)+1)[2:]).encode('ascii')
+                t = (hex(int(str(t)[2:-1],16)^(k+1)^k)[2:]).encode('ascii')
                 if len(t) == 1:
                     t = b'0' + t
                 if len(t) == 3:
-                    print("oh no")
-                    return -1
+                    t = t[1:]
                 c = c[:-3+-2*i] + t + c[-1+-2*i:] 
-            else:
+            elif i < 15+16:
                 print(c)
                 for j in range(i-15):
-                    j += 16
-                    t = c[-3 + -2*i:-2 + -2*i] + c[-2 + -2*i:-1 + -2*i]
-                    t = (hex(int(str(t)[2:-1],16)+1)[2:]).encode('ascii')
+                    j = j + 16
+                    t = c[-3 + -2*j:-2 + -2*j] + c[-2 + -2*j:-1 + -2*j]
+                    t = (hex(int(str(t)[2:-1],16)^(i-14)^(i-15))[2:]).encode('ascii')
                     if len(t) == 1:
                         t = b'0' + t
                     if len(t) == 3:
-                        t = t[1:]
-                    c = c[:-3+-2*i] + t + c[-1+-2*i:] 
-                    print("AAA")
-                    print(c)
+                        return -1
+                    c = c[:-3+-2*j] + t + c[-1+-2*j:]
+                print(c)
                 break
-    return c, decrypt(c)
-
-
-
+            else:
+                break
+    return c, decrypt(c[:-1])
