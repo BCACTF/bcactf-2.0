27706836baed42ff13c284fdb45ef9cb1dc09806
Anthony Li <me@anli.dev>
2021-06-11 08:24:02 -0400

    CSP: The Try/Catch Update

--- csp/server/server.js
+++ csp/server/server.js
@@ -43,55 +43,88 @@ app.post("/visit", async (req, res) => {
         res.json({visited: true});
     } catch (e) {
         console.error(e);
-        res.status(500);
-        res.json({error: true});
+        try {
+            res.status(500);
+            res.json({ error: true });
+        } catch (e) { }
     }
 });
 
 app.post("/publish", (req, res) => {
-    db.insert({...req.body, created: new Date(), cspViolations: 0, cspChars: 0}, (error, page) => {
-        if (error) {
-            console.error(error);
+    try {
+        db.insert({ ...req.body, created: new Date(), cspViolations: 0, cspChars: 0 }, (error, page) => {
+            if (error) {
+                console.error(error);
+                res.status(500);
+                res.json({ error: true });
+            } else {
+                res.json({ id: page._id });
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        try {
             res.status(500);
-            res.json({error: true});
-        } else {
-            res.json({id: page._id});
-        }
-    });
+            res.json({ error: true });
+        } catch (e) {}
+    }
 });
 
 app.post("/report-csp-violation", (req, res) => {
-    if (!req.query || typeof req.query.id !== "string") return res.status(400).send("id must be a string");
-    if (typeof req.body !== "object" || typeof req.body["csp-report"] !== "object") return res.status(400).send("not a csp report");
-    db.update({_id: req.query.id}, {$inc: {cspViolations: 1, cspChars: req.body["csp-report"]["script-sample"]?.length || 0}}, {}, (error, count, _) => {
-        if (error) {
-            console.error(error);
-            res.sendStatus(500);
-        } else if (count > 0) {
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(404);
-        }
-    });
+    try {
+        if (!req.query || typeof req.query.id !== "string") return res.status(400).send("id must be a string");
+        if (typeof req.body !== "object" || typeof req.body["csp-report"] !== "object") return res.status(400).send("not a csp report");
+        db.update({ _id: req.query.id }, { $inc: { cspViolations: 1, cspChars: req.body["csp-report"]["script-sample"]?.length || 0 } }, {}, (error, count, _) => {
+            if (error) {
+                console.error(error);
+                res.sendStatus(500);
+            } else if (count > 0) {
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(404);
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        try {
+            res.status(500);
+            res.json({ error: true });
+        } catch (e) { }
+    }
 });
 
 app.get("/page/:id", (req, res) => {
-    db.findOne({_id: req.params.id}, (error, page) => {
-        if (error) {
-            console.error(error);
-            res.sendStatus(500);
-        } else if (page) {
-            res.type("html");
-            const violationContent = page.cspViolations > 0 ? `<p>Our Completely Secure Publishing system has blocked <strong>${page.cspViolations}</strong> hacking attempt${page.cspViolations === 1 ? "" : "s"} on this page${page.cspChars > 0 ? ` totalling ${page.cspChars}+ characters` : ""}.</p>` : "";
-            const adminContent = req.cookies.secret === secret ? `<p>Prize: ${flag}</p>` : "<p>You must be an administrator to judge the writing competition.</p>";
-            const html = `<!DOCTYPE html><html><head><meta charset="utf-8"><title>${page.title || "Untitled"}</title></head><body>${violationContent}${page.content || ""}${adminContent}</body></html>`;
-            res.set("Content-Security-Policy", `child-src 'none'; connect-src 'none'; default-src 'none'; font-src 'none'; frame-src 'none'; img-src 'none'; manifest-src 'none'; media-src 'none'; object-src 'none'; prefetch-src 'none'; script-src 'report-sample'; style-src 'report-sample'; worker-src 'none'; report-uri /report-csp-violation?id=${req.params.id}`);
-            res.type("html");
-            res.send(html);
-        } else {
-            res.sendStatus(404);
-        }
-    });
+    try {
+        db.findOne({ _id: req.params.id }, (error, page) => {
+            try {
+                if (error) {
+                    console.error(error);
+                    res.sendStatus(500);
+                } else if (page) {
+                    res.type("html");
+                    const violationContent = page.cspViolations > 0 ? `<p>Our Completely Secure Publishing system has blocked <strong>${page.cspViolations}</strong> hacking attempt${page.cspViolations === 1 ? "" : "s"} on this page${page.cspChars > 0 ? ` totalling ${page.cspChars}+ characters` : ""}.</p>` : "";
+                    const adminContent = req.cookies.secret === secret ? `<p>Prize: ${flag}</p>` : "<p>You must be an administrator to judge the writing competition.</p>";
+                    const html = `<!DOCTYPE html><html><head><meta charset="utf-8"><title>${page.title || "Untitled"}</title></head><body>${violationContent}${page.content || ""}${adminContent}</body></html>`;
+                    res.set("Content-Security-Policy", `child-src 'none'; connect-src 'none'; default-src 'none'; font-src 'none'; frame-src 'none'; img-src 'none'; manifest-src 'none'; media-src 'none'; object-src 'none'; prefetch-src 'none'; script-src 'report-sample'; style-src 'report-sample'; worker-src 'none'; report-uri /report-csp-violation?id=${req.params.id}`);
+                    res.type("html");
+                    res.send(html);
+                } else {
+                    res.sendStatus(404);
+                }
+            } catch (e) {
+                try {
+                    res.status(500);
+                    res.json({ error: true });
+                } catch (e) { }
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        try {
+            res.status(500);
+            res.json({ error: true });
+        } catch (e) { }
+    }
 });
 
 app.use(express.static(join(__dirname, "../public")));
