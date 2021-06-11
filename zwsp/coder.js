42f0987dc4daea4ca6fdf7761c6a86ce67f78ca9
theberge-256 <68395886+theberge-256@users.noreply.github.com>
2021-06-11 10:54:48 -0400

    unflipped bits

--- zwsp/coder.js
+++ zwsp/coder.js
@@ -10,14 +10,14 @@ const encode = (str) => {
 
   let out = "";
   for (let i = 0; i < buf.length; ++i) {
-    out += (ZWSP_BIT_TABLE[buf[i] & 1] +
-      ZWSP_BIT_TABLE[(buf[i] >> 1) & 1] +
-      ZWSP_BIT_TABLE[(buf[i] >> 2) & 1] +
-      ZWSP_BIT_TABLE[(buf[i] >> 3) & 1] +
-      ZWSP_BIT_TABLE[(buf[i] >> 4) & 1] +
-      ZWSP_BIT_TABLE[(buf[i] >> 5) & 1] +
+    out += (ZWSP_BIT_TABLE[(buf[i] >> 7) & 1] +
       ZWSP_BIT_TABLE[(buf[i] >> 6) & 1] +
-      ZWSP_BIT_TABLE[(buf[i] >> 7) & 1]);
+      ZWSP_BIT_TABLE[(buf[i] >> 5) & 1] +
+      ZWSP_BIT_TABLE[(buf[i] >> 4) & 1] +
+      ZWSP_BIT_TABLE[(buf[i] >> 3) & 1] +
+      ZWSP_BIT_TABLE[(buf[i] >> 2) & 1] +
+      ZWSP_BIT_TABLE[(buf[i] >> 1) & 1] +
+      ZWSP_BIT_TABLE[buf[i] & 1]);
   }
 
   return out;
@@ -28,7 +28,7 @@ const decode = (str) => {
   const out = new Uint8Array(bits.length >> 3);
 
   for (let i = 0; i < bits.length; ++i) {
-    out[i >> 3] |= REVERSE_BIT_TABLE[bits[i]] << (i & 0x7);
+    out[i >> 3] |= REVERSE_BIT_TABLE[bits[i]] << ((i & 0x7) ^ 0x7);
   }
 
   return new TextDecoder().decode(out);
